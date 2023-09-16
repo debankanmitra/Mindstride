971cbe58a337b7495cfdb6567923ba6c3ad6d7a9
debankanmitra <debankanmitra@gmail.com>
2023-09-16 11:34:30 +0530

    legalassistant

---
+++ legalassistant/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Navbar from './nav.jsx'
+
+function App() {
+
+  return (
+    <>
+    <Navbar/>
+
+    </>
+  )
+}
+
+export default App
---
+++ legalassistant/src/nav.jsx
@@ -0,0 +1,16 @@
+function nav() {
+  return (
+    <>
+    <nav>
+        
+        <li>About</li>
+        <li>How it works</li>
+        <li>Contact</li>
+        
+    </nav>
+    
+    </>
+  )
+}
+
+export default nav;
