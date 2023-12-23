fdca9bdacf762c14e24bbb39e5539b55aa3d9b3d
debankanmitra <debankanmitra@gmail.com>
2023-12-23 22:25:08 +0530

    fsd

--- backend/embeddings.py
+++ backend/embeddings.py
@@ -1,23 +1,31 @@
 # Do embeddings of textual data in a vector space like Pinecone
 # https://youtu.be/ySus5ZS0b94?si=Ml_ExkivVtDLAzKe
+# https://colab.research.google.com/github/openai/openai-cookbook/blob/master/examples/vector_databases/pinecone/Semantic_Search.ipynb#scrollTo=Hg0yIGu3tRNo
+# https://colab.research.google.com/github/openai/openai-cookbook/blob/master/examples/vector_databases/pinecone/Gen_QA.ipynb#scrollTo=k7ut_DNBwIk1
+
 
 import os
+from dotenv import load_dotenv
 import pinecone
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Pinecone
 from text_processing import extract_text_from_pdf, transform_text
+
+load_dotenv()
 
 PINECONE_API_KEY=os.getenv("PINECONE_API_KEY")
 PINECONE_ENV=os.getenv("PINECONE_ENV")
 PINECONE_INDEX=os.getenv("PINECONE_INDEX")
 PINECONE_METRIC=os.getenv("PINECONE_METRIC")
 PINECONE_DIMENSION=os.getenv("PINECONE_DIMENSION")
+TEXT_EMBEDDING_MODEL=os.getenv("TEXT_EMBEDDING_MODEL")
 
 # initialize pinecone Datastore
 pinecone.init(
     api_key=PINECONE_API_KEY,  # find at app.pinecone.io
     environment=PINECONE_ENV,  # next to api key in console
 )
+# implement the whole without langchain
 
 # First, check if our index already exists. If it doesn't, we create it
 if PINECONE_INDEX not in pinecone.list_indexes():
@@ -25,19 +33,24 @@ if PINECONE_INDEX not in pinecone.list_indexes():
     pinecone.create_index(
       name=PINECONE_INDEX,
       metric=PINECONE_METRIC,
-      dimension=PINECONE_DIMENSION  
+      dimension=PINECONE_DIMENSION,
 )
 
-# Loading pdf, extracting text ,concatenating and transforming text
-pdf_texts = extract_text_from_pdf("cow.pdf")
+index = pinecone.GRPCIndex(PINECONE_INDEX)
+# index.upsert()
 
+
+# Loading pdf, extracting text ,concatenating and transforming text
+pdf_texts = extract_text_from_pdf("zebra.pdf")
 texts = transform_text(pdf_texts)
 
 
-# Text embeddings in vector space
-embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
 
-# The from_texts() function in Pinecone's vector store is used to upload both text documents and 
-# their corresponding embedding vectors to a specified Pinecone index.
-# this database works like a sql database there will be two columns (text and embedding)
+# # Text embeddings in vector space
+embeddings = OpenAIEmbeddings(model=TEXT_EMBEDDING_MODEL)
+
+
+# # The from_texts() function in Pinecone's vector store is used to upload both text documents and 
+# # their corresponding embedding vectors to a specified Pinecone index.
+# # this database works like a sql database there will be two columns (text and embedding)
 vectorstore = Pinecone.from_texts(texts,embeddings,index_name=PINECONE_INDEX)
--- backend/text_processing.py
+++ backend/text_processing.py
@@ -1,9 +1,18 @@
 ## extract and Processes text from pdf
 
+import os
 import re
+from dotenv import load_dotenv
+import openai
 from PyPDF2 import PdfReader
 from text_encoding import tiktoken_len
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+
+load_dotenv()
+
+TEXT_EMBEDDING_MODEL=os.getenv("TEXT_EMBEDDING_MODEL")
+OPENAI_API_KEY=os.getenv("OPENAI_API_KEY")
 
 # Text Extraction
 def extract_text_from_pdf(pdf_path):
@@ -18,14 +27,32 @@ def extract_text_from_pdf(pdf_path):
 
 # Cleaning and Normalization
 def clean_text(text):
+    # remove newlines that occur in the middle of sentences
+    # removed_extraline_texts = re.compile(r"\n(?=[a-z])").sub("", text)
+
+    # Removing hyphens that are at the end of lines
+    cleaned_text = re.sub(r'(\w+)-\n(\w+)', r'\1\2', text)
+
+    # Remove extra whitespaces
+    cleaned_text = re.sub(r'\s+', ' ', cleaned_text)
+
+    # Remove repeated characters (e.g., "helloooo" becomes "hello")
+    cleaned_text = re.sub(r'(.)\1{2,}', r'\1', cleaned_text)
+
+    # Remove leading and trailing spaces
+    cleaned_text = cleaned_text.strip()
+
     # Remove special characters and punctuation
-    text = re.sub(r'[^a-zA-Z0-9\s]', '', text)
+    text = re.sub(r'[^a-zA-Z0-9\s]', '', cleaned_text)
+
     # Convert to lowercase
     text = text.lower()
+    
     return text
 
 def text_replace(texts):
     cleaned_texts = [text.replace("\n", " ") for text in texts]
+    cleaned_texts = [re.sub(r'\s+', ' ', text) for text in cleaned_texts]
     return cleaned_texts
 
 
@@ -46,3 +73,9 @@ def transform_text(text):
     chunks= text_splitter.split_text(text)
 
     return text_replace(chunks)
+
+# experimental
+def get_embeddings(texts):
+    openai.api_key = OPENAI_API_KEY
+    embeddings = openai.Embedding.create(input=texts, engine=TEXT_EMBEDDING_MODEL).data[0].embedding
+    return embeddings
