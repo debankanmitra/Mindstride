797105a3d347a4f6e0a8c0b362f6345bfe2d8943
debankanmitra <debankanmitra@gmail.com>
2023-12-23 22:27:23 +0530

    sdf

--- backend/embeddings(without langchain).py
+++
@@ -1,3 +0,0 @@
-# https://colab.research.google.com/github/openai/openai-cookbook/blob/master/examples/vector_databases/pinecone/Semantic_Search.ipynb#scrollTo=LI76-BMFhUmS
-# https://platform.openai.com/docs/guides/embeddings
-# https://docs.pinecone.io/docs/openai
--- backend/embeddings.py
+++ backend/embeddings.py
@@ -2,7 +2,9 @@
 # https://youtu.be/ySus5ZS0b94?si=Ml_ExkivVtDLAzKe
 # https://colab.research.google.com/github/openai/openai-cookbook/blob/master/examples/vector_databases/pinecone/Semantic_Search.ipynb#scrollTo=Hg0yIGu3tRNo
 # https://colab.research.google.com/github/openai/openai-cookbook/blob/master/examples/vector_databases/pinecone/Gen_QA.ipynb#scrollTo=k7ut_DNBwIk1
-
+# https://colab.research.google.com/github/openai/openai-cookbook/blob/master/examples/vector_databases/pinecone/Semantic_Search.ipynb#scrollTo=LI76-BMFhUmS
+# https://platform.openai.com/docs/guides/embeddings
+# https://docs.pinecone.io/docs/openai
 
 import os
 from dotenv import load_dotenv
